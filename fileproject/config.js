114349f6a09c5087e2bab58460f6ad2c23076c74
mittamahesh <mittamahesh12@gmail.com>
2024-07-14 22:31:04 +0530

    url update

--- fileproject/config.js
+++ fileproject/config.js
@@ -1,5 +1,5 @@
 const API_URL = process.env.NODE_ENV === 'production'
-  ? 'https://personaskill-portal.onrender.com/'  // Replace with your Render URL
+  ? 'https://personaskill-portal.onrender.com'  // Replace with your Render URL
   : 'http://localhost:5000';
 
 export default API_URL;
